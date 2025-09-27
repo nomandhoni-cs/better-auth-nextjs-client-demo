712263ca5a637e200c702fc19f3605a793f8b326
Noman Dhoni <nomandhoni@yahoo.com>
2025-09-27 22:41:56 +0600

    Jwk token add

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -144,8 +144,8 @@ export default function Dashboard() {
             </CardContent>
             </Card>
       </div> */}
-      <JWTClient />
       <JwtDemoPage />
+      <JWTClient />
     </>
   )
 }
